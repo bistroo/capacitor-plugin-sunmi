4b8fc944fde90d60b8818005347dfa0d3f386889
Youri van Mill <me@youri.dev>
2023-03-09 13:24:29 +0100

    feat: add error handling, add event listener for printer status

--- android/src/main/java/io/bistroo/plugins/SunmiPlugin.java
+++ android/src/main/java/io/bistroo/plugins/SunmiPlugin.java
@@ -1,5 +1,7 @@
 package io.bistroo.plugins;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
@@ -36,163 +38,191 @@ public class SunmiPlugin extends Plugin {
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void raw(PluginCall call) throws RemoteException {
-        String value = call.getString("value");
+        if (mPrinterService != null) {
+            String value = call.getString("value");
 
-        mPrinterService.sendRAWData(Base64.decode(value, Base64.DEFAULT), null);
+            mPrinterService.sendRAWData(Base64.decode(value, Base64.DEFAULT), null);
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void table(PluginCall call) throws RemoteException, JSONException {
-        JSArray objects = call.getArray("rows");
+        if (mPrinterService != null) {
+            JSArray objects = call.getArray("rows");
 
-        ArrayList<String> rows = new ArrayList<>();
-        ArrayList<Integer> sizes = new ArrayList<>();
-        ArrayList<Integer> alignments = new ArrayList<>();
+            ArrayList<String> rows = new ArrayList<>();
+            ArrayList<Integer> sizes = new ArrayList<>();
+            ArrayList<Integer> alignments = new ArrayList<>();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            for (int i = 0; i < objects.length(); i++) {
-                rows.add(objects.getJSONObject(i).getString("value"));
-                sizes.add(objects.getJSONObject(i).getInt("size"));
-                alignments.add(objects.getJSONObject(i).getInt("alignment"));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                for (int i = 0; i < objects.length(); i++) {
+                    rows.add(objects.getJSONObject(i).getString("value"));
+                    sizes.add(objects.getJSONObject(i).getInt("size"));
+                    alignments.add(objects.getJSONObject(i).getInt("alignment"));
+                }
+
+                mPrinterService.printColumnsText(
+                        rows.toArray(new String[rows.size()]),
+                        sizes.stream().mapToInt(i -> i).toArray(),
+                        alignments.stream().mapToInt(i -> i).toArray(),
+                        null
+                );
             }
 
-            mPrinterService.printColumnsText(
-                    rows.toArray(new String[rows.size()]),
-                    sizes.stream().mapToInt(i->i).toArray(),
-                    alignments.stream().mapToInt(i->i).toArray(),
-                    null
-            );
+            call.resolve();
         }
-
-        call.resolve();
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void line(PluginCall call) throws RemoteException {
-        String text = call.getString("text");
-        Boolean wrap = call.getBoolean("wrap");
+        if (mPrinterService != null) {
+            String text = call.getString("text");
+            Boolean wrap = call.getBoolean("wrap");
 
-        if (text != null) {
-            mPrinterService.printText(text, null);
+            if (text != null) {
+                mPrinterService.printText(text, null);
+            }
+
+            if (wrap != null && wrap) {
+                mPrinterService.lineWrap(1, null);
+            }
+
+            call.resolve();
         }
-
-        if (wrap != null && wrap) {
-            mPrinterService.lineWrap(1, null);
-        }
-
-        call.resolve();
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void wrap(PluginCall call) throws RemoteException {
-        mPrinterService.lineWrap(1, null);
+        if (mPrinterService != null) {
+            mPrinterService.lineWrap(1, null);
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void text(PluginCall call) throws RemoteException {
-        mPrinterService.printText(call.getString("text"), null);
+        if (mPrinterService != null) {
+            mPrinterService.printText(call.getString("text"), null);
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void bold(PluginCall call) throws RemoteException {
-        mPrinterService.sendRAWData(new byte[]{0x1B, 0x45, 0x1}, null);
+        if (mPrinterService != null) {
+            mPrinterService.sendRAWData(new byte[]{0x1B, 0x45, 0x1}, null);
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void normal(PluginCall call) throws RemoteException {
-        mPrinterService.sendRAWData(new byte[]{0x1B, 0x45, 0x0}, null);
+        if (mPrinterService != null) {
+            mPrinterService.sendRAWData(new byte[]{0x1B, 0x45, 0x0}, null);
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void align(PluginCall call) throws RemoteException {
-        switch (call.getString("direction")) {
-            case "CENTER":
-                mPrinterService.setAlignment(1, null);
-                break;
-            case "RIGHT":
-                mPrinterService.setAlignment(2, null);
-                break;
-            default:
-                mPrinterService.setAlignment(0, null);
-        }
+        if (mPrinterService != null) {
+            switch (call.getString("direction")) {
+                case "CENTER":
+                    mPrinterService.setAlignment(1, null);
+                    break;
+                case "RIGHT":
+                    mPrinterService.setAlignment(2, null);
+                    break;
+                default:
+                    mPrinterService.setAlignment(0, null);
+            }
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void start(PluginCall call) throws RemoteException {
-        mPrinterService.enterPrinterBuffer(true);
+        if (mPrinterService != null) {
+            mPrinterService.enterPrinterBuffer(true);
 
-        call.resolve();
+            call.resolve();
+        }
     }
 
-    @PluginMethod
+    @PluginMethod()
     public void print(PluginCall call) throws RemoteException {
-        mPrinterService.commitPrinterBufferWithCallback(new InnerResultCallback() {
-            @Override
-            public void onRunResult(boolean isSuccess) {
-                Log.d(TAG, String.format("%b", isSuccess));
+        if (mPrinterService != null) {
+            if (mPrinterService.updatePrinterState() != 1) {
+                Log.e(TAG, String.format("Failed with printer status: %d", mPrinterService.updatePrinterState()));
+
+                call.reject(String.valueOf(mPrinterService.updatePrinterState()));
+
+                return;
             }
 
-            @Override
-            public void onReturnString(String result) {
-                Log.d(TAG, result);
-            }
+            mPrinterService.commitPrinterBufferWithCallback(new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) {
+                    Log.d(TAG, String.format("InnerResultCallback#onRunResult() %b", isSuccess));
+                }
 
-            @Override
-            public void onRaiseException(int code, String msg) {
-                Log.e(TAG, String.format("%s, %d", msg, code));
+                @Override
+                public void onReturnString(String result) {
+                    Log.d(TAG, String.format("InnerResultCallback#onReturnString() %s", result));
+                }
 
-                call.reject(msg, String.valueOf(code));
-            }
-
-            @Override
-            public void onPrintResult(int code, String msg) {
-                if (code == 0) {
-                    Log.d(TAG, msg);
-
-                    call.resolve();
-                } else {
-                    Log.e(TAG, String.format("%s, %d", msg, code));
+                @Override
+                public void onRaiseException(int code, String msg) {
+                    Log.e(TAG, String.format("InnerResultCallback#onRaiseException() %s, %d", msg, code));
 
                     call.reject(msg, String.valueOf(code));
                 }
-            }
-        });
 
-        call.resolve();
+                @Override
+                public void onPrintResult(int code, String msg) {
+                    if (code == 0) {
+                        Log.d(TAG, String.format("InnerResultCallback#onPrintResult() %s, %d", msg, code));
+
+                        call.resolve();
+                    } else {
+                        Log.e(TAG, String.format("InnerResultCallback#onPrintResult() %s, %d", msg, code));
+
+                        call.reject(msg, String.valueOf(code));
+                    }
+                }
+            });
+        }
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void image(PluginCall call) throws RemoteException {
-        byte[] decodedString = Base64.decode(call.getString("image"), Base64.DEFAULT);
-        Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        if (mPrinterService != null) {
+            byte[] decodedString = Base64.decode(call.getString("image"), Base64.DEFAULT);
+            Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-        mPrinterService.printBitmap(image, null);
-
-        call.resolve();
+            mPrinterService.printBitmap(image, null);
+        }
     }
 
     @PluginMethod
     public void deviceInfo(PluginCall call) throws RemoteException {
-        String serialNumber = mPrinterService.getPrinterSerialNo();
-        String model = mPrinterService.getPrinterModal();
+        if (mPrinterService != null) {
+            String serialNumber = mPrinterService.getPrinterSerialNo();
+            String model = mPrinterService.getPrinterModal();
 
-        JSObject data = new JSObject();
-        data.put("serial_number", serialNumber);
-        data.put("model", model);
+            JSObject data = new JSObject();
+            data.put("serial_number", serialNumber);
+            data.put("model", model);
 
-        call.resolve(data);
+            call.resolve(data);
+        }
     }
 
     @Override
@@ -222,6 +252,16 @@ public class SunmiPlugin extends Plugin {
         filter.addAction("woyou.aidlservice.jiuv5.FIRMWARE_FAILURE_ACITON");
         filter.addAction("woyou.aidlservice.jiuv5.PRINTER_NON_EXISTENT_ACITON");
         filter.addAction("woyou.aidlservice.jiuv5.BLACKLABEL_NON_EXISTENT_ACITON");
+
+        class SunmiBroadcastReceiver extends BroadcastReceiver {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                JSObject ret = new JSObject();
+                ret.put("status", intent.getAction());
+
+                notifyListeners("printer-state", ret);
+            }
+        }
 
         getContext().registerReceiver(new SunmiBroadcastReceiver(), filter);
 
